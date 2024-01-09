bab5767f3d4cbabf8431f383b7b6e80c64f04746
laifubin <laifubin@zehui.com>
2024-01-09 17:12:26 +0800

    feat: ui init

---
+++ background.js
@@ -0,0 +1,143 @@
+
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if(message.isValid) {
+      if(message.type === 'fetchSellerSpirit') {
+        login('2022US2', message.p)
+        // fetchSellerSpirit(message.data.join(',')).then(res => {
+        //   sendResponse(res?.data ?? [])
+        // })
+      } 
+    } else {
+      createNotification('提示', '试用已到期~')
+      sendResponse([])
+    }
+    return true
+  }
+)
+
+const result = []
+function fetchOriginHtml(asin) {
+  const url = 'https://www.amazon.com/dp/' + asin + '?psc=1&_=' + Date.now()
+  return fetch(url)
+    .then(response => response.text())
+    .then(data => {
+      const item = {}
+      const title = data.replace(/.*<title>Amazon.com: (.*) : Electronics<\/title>/, '$1')
+      const listing = title.split(/\s+/)[1]
+
+      const startString = data.replace(/<span id="acrPopover".*title="(.+)">/g, '$1')
+      const start = startString?.split(/[\sa-z]+/).filter(Boolean).join('/')
+      
+      const infos = ['ASIN', 'Package Dimensions', 'Item Weight', 'Customer Reviews', 'Date First Available']
+      infos.forEach(info => {
+        const reg = new RegExp(`.*${info}.*?<td.*?> (.*?) <\/td>.*`, 'g')
+        item[info] = data.replace(reg, '$1')
+      })
+      
+      // const size = data.replace(/.*Package Dimensions.*?<td.*?> (.*?) <\/td>.*/, '$1')
+      console.log("响应数据：", infos, start, listing);
+      return { ...item, infos, start, listing }
+    })
+    .catch(error => {
+      console.error("获取响应数据时发生错误：", error);
+    });
+}
+
+function fetchSellerSpirit (asins) {
+  const token = getStorage('mjjl_token') 
+  // const url = "https://www.sellersprite.com/v2/extension/competitor-lookup/quick-view/US?asins=B0BQ356JNP&source=chrome&miniMode=false&withRelation=true&withSaleTrend=false&tk=642232.1037917&version=4.1.1&language=zh_CN&extension=lnbmbgocenenhhhdojdielgnmeflbnfb"
+  const url = `https://www.sellersprite.com/v2/extension/asin/US?asins=${asins}&tk=412968.22477&version=4.1.1&language=zh_CN&extension=lnbmbgocenenhhhdojdielgnmeflbnfb&source=chrome`
+  return fetch(url, {
+    headers: {
+      authority: 'www.sellersprite.com',
+      scheme: 'https',
+      accept: 'application/json',
+      'accept-encoding': 'gzip, deflate, br',
+      'accept-language': 'zh-CN,zh;q=0.9',
+      'auth-token': token,
+      'cache-control': 'no-cache',
+      'content-type': 'application/json',
+      cookie: 'ecookie=S0tSgmhgJaEqxkvC_CN; JSESSIONID=29C08AA0B891695385EE733916313D13; _ga=GA1.1.920354382.1689817159; _ga_38NCVF2XST=GS1.1.1689817159.1.0.1689817159.0.0.0',
+      pragma: 'no-cache',
+      'random-token': '811081a2-3a82-402d-9619-87a10a6c7813',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'none',
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+    },
+    credentials: 'include'
+  })
+  .then(res => {
+   
+    // ERR_NEED_RE_AUTHORIZED
+    const data = res.text()
+    if(typeof data === 'object') {
+      data.then(res => {
+        const data = JSON.parse(res.text())
+        if(data.code === 'ERR_NEED_RE_AUTHORIZED') {
+          console.log("===============");
+          // login()
+        }
+      })
+    } else {
+      return JSON.parse(data)
+    }
+  })
+  .catch((e) => {
+    console.log("eeeeeeeeeeeeee", e);
+  })
+}
+
+
+function login (email, password) {
+  return fetch(`https://www.sellersprite.com/v2/extension/signin?email=${email}&password=${password}&tk=544990.936507&version=4.1.1&language=zh_CN&extension=lnbmbgocenenhhhdojdielgnmeflbnfb&source=chrome`, {
+    "headers": {
+      "accept": "application/json",
+      "accept-language": "zh-CN,zh;q=0.9",
+      "cache-control": "no-cache",
+      "content-type": "application/json",
+      "pragma": "no-cache",
+      "random-token": "811081a2-3a82-402d-9619-87a10a6c7813",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "none"
+    },
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": null,
+    "method": "GET",
+    "mode": "cors",
+    "credentials": "include"
+  }).then(res => res.json())
+  .then(res => {
+    setStorage('mjjl_email', res.data?.email)
+    setStorage('mjjl_token', res.data?.token)
+    return res
+  })
+}
+
+
+
+function getStorage(key) {
+  let result
+  chrome.storage.local.get([key], function(result) {
+    result = result.key
+  });
+  return result
+}
+
+function setStorage(key, value) {
+  chrome.storage.local.set({[key]: value}, function(result) {
+  })
+}
+
+function createNotification(title, message) {
+  const notificationOptions = {
+    type: "basic",
+    iconUrl: "am.png",
+    title,
+    message
+  };
+
+  chrome.notifications.create('AMHelper' + Date.now(), notificationOptions);
+}
--- js/contentScript.js
+++ js/contentScript.js
@@ -1,20 +1,38 @@
 (function(){
+  /** 引入SheetJS */
+  // const scriptEl = document.createElement('script')
+  // scriptEl.lang = "javascript"
+  // scriptEl.src = 'https://cdn.sheetjs.com/xlsx-0.20.0/package/dist/xlsx.full.min.js'
+  // document.head.appendChild(scriptEl)
+  // scriptEl.onload = () => {
+  //   window.XLSX = XLSX
+  // }
+ 
+
   /** 将浮窗添加到页面中 */
   const helperHtml = `
     <div id="main">
       <h3 class="title">Amazon Helper</h3>
       <div class="content">
+
+        <div class="lines"></div>
+        <div class="diyBtn">
+          <span id="currentPage">获取当前页</span>
+          <span id="2">获取当前查询所有结果</span>
+        </div>
+        <div class="lines"></div>
+
         <div class="form-item">
-          <label for="keyword">关键词</label>
-          <input type="text" id="keyword">
+          <label for="keywords">关键词</label>
+          <input type="text" id="keywords" placeholder="多个以英文逗号隔开">
         </div>
         <div class="form-item">
           <label for="link">链接</label>
-          <input type="text" id="link">
+          <input type="text" id="link" placeholder="请输入链接">
         </div>
-        <input type="button" value="提交">
+        <input id="submit2" type="button" value="提交">
       </div>
-      <span class="author">@22023 by laifubin</span>
+      <span class="author">copyright@laifubin by 2023</span>
     </div>
     <div class="close"><span><<</span></div>
   `
@@ -38,4 +56,68 @@
       e.target.style.textAlign = 'left'
     }
   })
+
+  /** 点击提交 */
+  const currentPageEl = document.querySelector('#am-helper #currentPage')
+  const lastDate = +new Date('2023-07-27 24:00:00')
+  currentPageEl.addEventListener('click', (e) => {
+    const nowDate = Date.now()
+    const isValid = nowDate - lastDate < 0
+
+    let asin = []
+    if(isValid) {
+      const asinEls = document.querySelectorAll('div[data-asin][data-component-type="s-search-result"]');
+      asin = Array.from(asinEls).map(el => el.getAttribute('data-asin'))
+    }
+    console.log(hex_md5('2US20211220'), 2323)
+    chrome.runtime.sendMessage({ type: 'fetchSellerSpirit', data: asin, isValid, p:hex_md5('2US20211220') }, (res) => {
+      if(res.length) {
+        console.log('very good', res)
+        const data = res.map(item => {
+          const { asin, title, dimensions, price, image, weight } = item
+          const listing = title.split(/\s+/)[1]
+          return [asin, listing, title, dimensions, price, image, weight]
+        })
+        downloadFile(createExecl(data), 'AMHelper.xlsx')
+      }
+    })
+  })
+
+
+
+  function createExecl (data = [], title = ['asin', 'listing', 'title', 'dimensions', 'price', 'image', 'weight']) {
+    // 模拟一些数据，这里使用二维数组表示表格数据
+    data.unshift(title)
+
+    // 创建一个工作簿对象
+    const workbook = XLSX.utils.book_new();
+
+    // 创建一个工作表对象
+    const worksheet = XLSX.utils.aoa_to_sheet(data);
+
+    // 将工作表添加到工作簿中
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+
+    // 将工作簿转换为Excel文件的二进制数据
+    const excelData = XLSX.write(workbook, { type: 'array' });
+    return excelData
+  }
+
+  function downloadFile(blobData, fileName = Date.now() + '.xlsx') {
+    // 创建一个Blob对象
+    const blob = new Blob([blobData], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+    // 创建一个下载链接
+    const url = URL.createObjectURL(blob);
+    // 创建一个链接元素，并模拟点击来触发下载
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName; // 指定下载文件的名称
+    document.body.appendChild(a);
+    a.click();
+
+    // 释放URL对象，以避免内存泄漏 c28d7fa457c4c6f893707dc376b83a95
+    // 2US20211220
+    URL.revokeObjectURL(url);
+  }
 })()
+
